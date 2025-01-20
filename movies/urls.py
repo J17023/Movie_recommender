900be5a19a96a60f38f55e1aaad445050673d8b7
J1723 <josejulian1723@gmail.com>
2025-01-20 00:16:03 -0500

    se arreglo error de la url del router

--- movies/urls.py
+++ movies/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import ModelViewSet
+from .views import movie_viewset
 
 router = DefaultRouter()
-router.register(r'movies', ModelViewSet, basename='movie')
+router.register(r'movies',movie_viewset)
 
 urlpatterns = [
     path('',include(router.urls)),
